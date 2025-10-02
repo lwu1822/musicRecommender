d384cbd6197028a5514a4813f7b033bf60b661fa
lwu1822 <lwu1822@gmail.com>
2025-10-02 09:41:40 -0700

    debug

--- backend/main.py
+++ backend/main.py
@@ -1,74 +1,140 @@
 import threading
-from flask import render_template, request, jsonify
-from flask_cors import CORS
-from flask_jwt_extended import JWTManager
 
-from __init__ import app, db
+# import "packages" from flask
+from flask import render_template  # import render_template from "public" flask libraries
+from flask_cors import CORS  # Import CORS here
+
+# import "packages" from "this" project
+from __init__ import app,db  # Definitions initialization
 from model.jokes import initJokes
-from model.users import initUsers
+from model.users import initSongs, initUsers
 from model.players import initPlayers
 
-# Import all your API blueprints
-from api.covid import covid_api
-from api.joke import joke_api
-from api.user import user_api
+
+# setup APIs
+from api.covid import covid_api # Blueprint import api definition
+from api.joke import joke_api # Blueprint import api definition
+from api.user import user_api # Blueprint import api definition
 from api.player import player_api
 from api.song import song_api
-from projects.projects import app_projects
 
-# Initialize the SQLAlchemy object
+
+# setup App pages
+from projects.projects import app_projects # Blueprint directory import projects definition
+
+
+""" 
+JWT test
+""" 
+
+from flask import jsonify, request, make_response, redirect, session
+import jwt 
+import datetime 
+from functools import wraps
+from flask_jwt_extended import (JWTManager, create_access_token, create_refresh_token, set_access_cookies, set_refresh_cookies)
+from flask_session import Session 
+import os
+#from __init__ import ApplicationConfig
+
+""" 
+"""
+
+
+# Initialize the SQLAlchemy object to work with the Flask app instance
 db.init_app(app)
 
-# --- ROBUST PREFLIGHT REQUEST HANDLER ---
-# This function will run before every request.
+# --- ADD THIS DEBUGGING BLOCK ---
 @app.before_request
-def before_request_handler():
-    # Checks if the request method is OPTIONS (a preflight request)
+def log_request_info():
+    app.logger.debug('Headers: %s', request.headers)
+    app.logger.debug('Body: %s', request.get_data())
     if request.method == 'OPTIONS':
-        # Creates a response with the necessary CORS headers
-        response = jsonify({})
-        response.headers.add('Access-Control-Allow-Origin', '*')
-        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-        response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-        return response
+        app.logger.info("Caught a preflight OPTIONS request!")
+# --- END DEBUGGING BLOCK ---
 
-# --- CORS CONFIGURATION FOR ACTUAL REQUESTS ---
-# This will handle CORS for all non-preflight requests.
-CORS(app, origins=["http://127.0.0.1:4000", "http://localhost:4000", "https://lwu1822.github.io"], supports_credentials=True)
-
-# Register all API blueprints
-app.register_blueprint(joke_api)
-app.register_blueprint(covid_api)
-app.register_blueprint(user_api)
+# register URIs
+app.register_blueprint(joke_api) # register api routes
+app.register_blueprint(covid_api) # register api routes
+app.register_blueprint(user_api) # register api routes
 app.register_blueprint(player_api)
-app.register_blueprint(app_projects)
-app.register_blueprint(song_api)
+app.register_blueprint(app_projects) # register app pages
+app.register_blueprint(song_api) 
 
-# --- JWT AND OTHER APP CONFIGURATION ---
 app.config['SECRET_KEY'] = 'secretkey'
 app.config['JWT_TOKEN_LOCATION'] = ['cookies']
-app.config['JWT_COOKIE_CSRF_PROTECT'] = False # Keep this disabled for now
+app.config['JWT_COOKIE_CSRF_PROTECT'] = False
+# app.config['JWT_CSRF_CHECK_FORM'] = True
+
 jwt = JWTManager(app)
 
-# --- Standard Flask Routes ---
-@app.errorhandler(404)
+# <<< START CORS CONFIGURATION >>>
+# Define the list of trusted origins (your frontends)
+origins = [
+    "http://127.0.0.1:4000",      # Your local frontend for testing
+    "http://localhost:4000",       # Also include localhost for good measure
+    "https://lwu1822.github.io"    # Your DEPLOYED frontend website
+]
+
+# Initialize CORS for the entire application
+CORS(app, origins=origins, supports_credentials=True)
+# <<< END CORS CONFIGURATION >>>
+
+
+""" 
+JWT test
+""" 
+
+
+app.config['SECRET_KEY'] = 'secretkey'
+app.config['JWT_TOKEN_LOCATION'] = ['cookies']
+app.config['JWT_COOKIE_CSRF_PROTECT'] = True
+app.config['JWT_CSRF_CHECK_FORM'] = True
+
+jwt = JWTManager(app)
+
+
+""" 
+""" 
+
+
+
+@app.errorhandler(404)  # catch for URL not found
 def page_not_found(e):
+    # note that we set the 404 status explicitly
     return render_template('404.html'), 404
 
-@app.route('/')
+@app.route('/')  # connects default URL to index() function
 def index():
     return render_template("index.html")
 
-@app.route('/table/')
+@app.route('/table/')  # connects /stub/ URL to stub() function
 def table():
     return render_template("table.html")
 
+
+
+@app.route('/testing')
+def testing():
+    access_token = create_access_token(identity=str("usertest"))
+    refresh_token = create_refresh_token(identity=str("usertest"))
+    resp = make_response(redirect("http://swifties.duckdns.org/", 302))
+    set_access_cookies(resp, access_token)
+    set_refresh_cookies(resp, refresh_token)
+    return jsonify( {
+        "id": access_token
+    })
+
+
+
 @app.before_first_request
-def activate_job():
+def activate_job():  # activate these items 
     initJokes()
     initUsers()
+    #initSongs()
     initPlayers()
 
-# --- Main execution point ---
+# this runs the application on the development server
 if __name__ == "__main__":
+    # CORS is now handled globally, so no need for a separate setup here.
+    #initSongs()
     app.run(debug=True, host="0.0.0.0", port="8086")
